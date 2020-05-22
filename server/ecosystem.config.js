2a96434f468da94e7e16fa310c017c3f77d70e07
Nick Carpenetti <nickcarpenetti@gmail.com>
2020-05-22 13:17:51 -0700

    clean up formatting in ecosystem.config.js

--- server/ecosystem.config.js
+++ server/ecosystem.config.js
@@ -1,9 +1,9 @@
 module.exports = {
-	apps : [{
-		name: "sp-backend",
-		script: "src/index.js",
-		env: {
-			NODE_ENV: "production",
-		},
-	}]
-}
+  apps: [{
+    name: 'sp-backend',
+    script: 'src/index.js',
+    env: {
+      NODE_ENV: 'production',
+    },
+  }],
+};
