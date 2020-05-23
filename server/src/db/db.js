387cf76f8648fd907602e462ed79480141ad400c
Nick Carpenetti <nickcarpenetti@gmail.com>
2020-05-23 13:03:21 -0700

    capable of interacting with mongoDB

--- server/src/db/db.js
+++ server/src/db/db.js
@@ -1,6 +1,42 @@
 require('dotenv').config();
+const mongoose = require('mongoose');
+const Fighter = require('./models/fighter');
+
+mongoose.connect(process.env.DB_URL,
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    })
+    .then(() => {
+      console.log('connected to mongoDB');
+    })
+    .catch((error) => console.log(error));
+
+const getFighterID = async (name, tier) => {
+  const doc = await Fighter.findOne({
+    name: name,
+    tier: tier,
+  }).exec((err) => {
+    if(err) throw err;
+  });
+
+  if(!doc) {
+    return null;
+  }
+
+  return doc._id;
+};
+
+const createFighter = async (name, tier) => {
+  const fighterEntry = new Fighter({name: name, tier: tier});
+  const res = await fighterEntry.save()
+      .catch((err) => {
+        throw err;
+      });
+  return res._id;
+};
 
 module.exports = {
-  getFighterStats,
-  submitMatch,
+  getFighterID,
+  createFighter,
 };
