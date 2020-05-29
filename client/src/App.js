af04231694fbe5330d2d437118e09560cd65c50e
Nick Carpenetti <nickcarpenetti@gmail.com>
2020-05-29 16:34:40 -0700

    lots of css prettifying

--- client/src/App.js
+++ client/src/App.js
@@ -64,22 +64,34 @@ class FighterData extends React.Component {
   }
 
   render() {
+
     if(!this.state.fighter) {
       return (
-        <h4 className="Fighter-Props">
-          {this.state.text}
-        </h4>
+        <div className="Fighter-Props">
+          <h2 className="Fighter-Name">
+            {this.state.text}
+          </h2>
+          <h4>--</h4>
+          <h4>--</h4>
+          <h4>--</h4>
+          <h4>--</h4>
+          <h4>--</h4>
+        </div>
       )
     }
+
+    let fighter = this.state.fighter;
+    let winRate = fighter.totalMatches === 0 ? 0 : fighter.totalWins / fighter.totalMatches;
     return (
       <div className="Fighter-Props">
-        <h2>
-          {this.state.fighter.name}({this.state.fighter.tier} Tier)
+        <h2 className="Fighter-Name">
+          {fighter.name}
         </h2>
-        <h4>{this.state.fighter.totalMatches}</h4>
-        <h4>{this.state.fighter.totalWins}</h4>
-        <h4>{this.state.fighter.currentStreak}</h4>
-        <h4>{this.state.fighter.bestStreak}</h4>
+        <h4>{fighter.tier}</h4>
+        <h4>{fighter.totalMatches}</h4>
+        <h4>{fighter.totalWins}({winRate}%)</h4>
+        <h4>{fighter.currentStreak}</h4>
+        <h4>{fighter.bestStreak}</h4>
       </div>
     )
   }
@@ -89,21 +101,24 @@ function App() {
   return (
     <div className="App">
       <div className="App-Header">
-        <h1>Salty Prophet</h1>
+        <img src="logo512.png" alt="salt" className="Title-Salt"/>
+        <h1 className="App-Title">Salty Prophet</h1>
+        <img src="logo512.png" alt="salt" className="Title-Salt"/>
       </div>
       <div className="App-Body">
-        <SaltybetMessage />
         <div className="Fighter-Stats-Zone">
           <FighterData idx={0} />
           <div className="Fighter-Prop-Names">
-            <h2>Fighter Stats</h2>
+            <h2>VS</h2>
+            <h4>Tier</h4>
             <h4>Matches Recorded</h4>
-            <h4>Wins</h4>
+            <h4>Wins(Winrate)</h4>
             <h4>Current Streak</h4>
             <h4>Best Streak</h4>
           </div>
           <FighterData idx={1} />
         </div>
+        <SaltybetMessage />
       </div>
       <div className="App-Footer">
         &#169; 2020 Nicholas Carpenetti
