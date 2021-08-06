ede5e7c13096ba02e3b1ddf67bbf0db0f613d634
Nick Carpenetti <nickcarpenetti@gmail.com>
2021-08-06 18:27:42 +0000

    update packages

--- server/ecosystem.config.js
+++ server/ecosystem.config.js
@@ -1,9 +1,13 @@
 module.exports = {
-  apps: [{
+  apps : [{
     name: 'sp-backend',
     script: 'src/index.js',
+    instances: 0,
+    exec_mode: 'cluster',
+    watch: true,
+    log_date_format: 'YYYY-MM-DD HH:mm Z',
     env: {
       NODE_ENV: 'production',
-    },
-  }],
-};
+    }
+  }]
+}
